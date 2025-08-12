4e55e13e026323f8cb807608638167e0d745d9e8
Mahamadou Kante <mahakantem@gmail.com>
2025-08-12 14:37:56 +0000

    editions page

--- css/2016_style/js/agency.js
+++ css/2016_style/js/agency.js
@@ -6,19 +6,12 @@
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('a.page-scroll').on('click', function(event) {
-        var href = this.getAttribute('href');
-        // Only handle pure same-page hash links (e.g., '#about')
-        if (!href || href.charAt(0) !== '#') {
-            return; // allow normal navigation
-        }
-        var $target = $(href);
-        if ($target.length) {
-            $('html, body').stop().animate({
-                scrollTop: $target.offset().top
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        }
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
     });
 });
 
@@ -27,40 +20,9 @@ $('body').scrollspy({
     target: '.navbar-fixed-top'
 })
 
-// Mobile: toggle dropdown manually on touch/click for collapsed navbar
-$('.navbar').on('touchstart click', 'a.dropdown-toggle', function(event) {
-    var isCollapsed = $('.navbar-toggle').is(':visible');
-    if (isCollapsed) {
-        event.preventDefault();
-        event.stopPropagation();
-        var $parent = $(this).parent();
-        $parent.toggleClass('open');
-        $parent.siblings('.open').removeClass('open');
-        return false;
-    }
-});
-
-// Close dropdown when tapping outside in collapsed mode
-$(document).on('touchstart click', function(event) {
-    var isCollapsed = $('.navbar-toggle').is(':visible');
-    if (isCollapsed) {
-        if (!$(event.target).closest('.navbar .dropdown').length) {
-            $('.navbar .dropdown.open').removeClass('open');
-        }
-    }
-});
-
-// Close the collapsed navbar only when a real link is selected (not dropdown toggles)
-$('.navbar-collapse.in').on('click', 'a', function(event) {
-    var $link = $(this);
-    // If it's a dropdown toggle, do not close
-    if ($link.is('.dropdown-toggle')) {
-        event.preventDefault();
-        event.stopPropagation();
-        return false;
-    }
-    // Otherwise close the menu
-    $('.navbar-toggle:visible').trigger('click');
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
 });
 
 $('div.modal').on('show.bs.modal', function() {
